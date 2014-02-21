4aab66c79985b184b8f4aa86697286e3c46db37e
Joonas Rouhiainen <joonas@meedoc.com>
2014-02-22 01:36:14 +0200

    Last open data in api/now.

--- src/isolkkariopen/history.clj
+++ src/isolkkariopen/history.clj
@@ -20,6 +20,18 @@
     (mq/sort (array-map :_id -1)) ; sorted from newest to oldest by ObjectId
     (mq/limit (settings :max-history-results))))
 
+(defn lastOpenQuery []
+  (first (mq/with-collection collection
+    (mq/find {:open true})
+    (mq/sort (array-map :_id -1))
+    (mq/limit 1))))
+
+(defn nowQuery []
+  (first (mq/with-collection collection
+    (mq/find {})
+    (mq/sort (array-map :_id -1))
+    (mq/limit 1))))
+
 (defn round-places [number decimals]
   (let [factor (expt 10 decimals)]
     (bigdec (/ (round (* factor number)) factor))))
@@ -28,25 +40,33 @@
   {:buzz (olocam/buzz currPic prevPic)
    :open (olocam/olkkari-open? currPic)})
 
+(defn remove-objId [objMap]
+  (dissoc objMap :_id))
+
 (defn entries []
   "Fetch entries from DB, customizing their display form"
   (defn add-timestamp [objMap]
     (assoc objMap :time (.getTime (:_id objMap))))
-  (defn remove-objId [objMap]
-    (dissoc objMap :_id))
   (defn add-pretty-buzz [objMap]
     (assoc objMap
       :buzzPretty
       (str (format "%.1f" (:buzz objMap)) " %")))
-
+  
   (map
     #(add-pretty-buzz (remove-objId (add-timestamp %)))
     (entriesMapQuery)))
 
+(defn last-open []
+  (remove-objId (:time lastOpenQuery)))
+
 (defn insert-entry [entry]
   (mc/insert collection (merge entry {:_id (ObjectId.)})))
 
-(defn now [] (first (entries)))
+(defn now []
+  (let [lopen (:time lastOpenQuery)]
+    (if (nil? lopen)
+      (remove-objId (nowQuery))
+      (assoc (nowQuery) :last-open (last-open)))))
 
 (defn fetch-as-entry! []
   (let [currPic (olocam/fetch-pic!) prevPic @prev-pic]
