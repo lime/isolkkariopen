864381d84041b80a2e247f252e1e6753525444e7
Joonas Rouhiainen <joonas@meedoc.com>
2014-02-26 22:34:15 +0200

    Show last open on page only when closed & last closed only when open.

--- src/isolkkariopen/template.clj
+++ src/isolkkariopen/template.clj
@@ -1,7 +1,26 @@
 (ns isolkkariopen.template
   (:require [hiccup.core :refer [html]]))
 
-(defn index [nowEntry]
+(def placeholder "no data :(")
+
+(defn- last-closed-info [entry]
+  (str
+    "Last closed: "
+    (if (clojure.string/blank? (:lastClosed entry))
+      placeholder
+      (:lastClosedAgo entry))))
+
+(defn- last-open-info [entry]
+  (str
+    "Last open: "
+    (if (clojure.string/blank? (:lastOpen entry))
+      placeholder
+      (:lastOpenAgo entry))))
+
+(defn- last-opposite-info [entry]
+  ((if (:open entry) last-closed-info last-open-info) entry))
+
+(defn index [now-entry]
   (html
     [:head
       [:title "Is Olkkari open?"]
@@ -11,20 +30,12 @@
         "Is "
         [:a {:href "http://www.athene.fi/toiminta/olocam/"} "Olkkari"] 
         " open?"]
-      [:h2 (if (:open nowEntry) "Yeah baby!" "Nope.")]
+      [:h2 (if (:open now-entry) "Yeah baby!" "Nope.")]
       [:p
         "Current buzz: "
-        (:buzzPretty nowEntry)
+        (:buzzPretty now-entry)
         [:br]
-        "Last open: "
-        (if (clojure.string/blank? (:lastOpen nowEntry))
-          "no data :("
-          (:lastOpenAgo nowEntry))
-        [:br]
-        "Last closed: "
-        (if (clojure.string/blank? (:lastClosed nowEntry))
-          "no data :("
-          (:lastClosedAgo nowEntry))]
+        (last-opposite-info now-entry)]
       [:ul
         [:li
           "API: "
