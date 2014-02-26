0d3b1bc77a76425986fce84089da83c934f3fcef
Joonas Rouhiainen <joonas@meedoc.com>
2014-02-26 21:10:59 +0200

    Configurable buzz multiplier, still capped at 100 %.

--- src/isolkkariopen/olocam.clj
+++ src/isolkkariopen/olocam.clj
@@ -33,8 +33,10 @@
       (count eqbf))))
 
 (defn buzz [pic1 pic2]
-  (float
-    (normed-inequality (databuffer pic1) (databuffer pic2))))
+  (min 1
+    (* (settings :buzz-multiplier)
+      (float
+        (normed-inequality (databuffer pic1) (databuffer pic2))))))
 
 (defn olkkari-open? [pic]
   (not= Color/black
--- src/isolkkariopen/settings.clj
+++ src/isolkkariopen/settings.clj
@@ -6,6 +6,7 @@
   :cam-url             "http://www.athene.fi/olocam/latest.jpg"
   :history-size        (-> 400 megabytes)
   
+  :buzz-multiplier     2
   :noise-tolerance     5
   :max-history-results 100
   :olkkari-open-x      500
